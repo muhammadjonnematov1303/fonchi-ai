c89e2830928dfc3b3e072051d3ecced203df0c47
muhammadjonnematov1303 <shukurjalolov48@gmail.com>
2026-05-28 09:48:39 +0500

    Set cost per image to 100 som

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ PAYMENT_NAME = os.getenv("PAYMENT_NAME", "Muhammadjon Nematov")
 
 PROXY_URL = os.getenv("PROXY_URL") or None
 
-COST_PER_IMAGE = 500        # 1 ta rasm uchun so'm
+COST_PER_IMAGE = 100        # 1 ta rasm uchun so'm
 TOPUP_AMOUNT   = 10000      # To'lov tasdiqlanganda qo'shiladigan so'm
 
 IMAGES_DIR = "images"
